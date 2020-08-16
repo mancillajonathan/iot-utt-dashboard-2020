71684c07a03041a08b6d1116e1a40cfca8c55ea0
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-08-16 00:26:26 -0700

    creating temperature div in weather component

--- src/pages/Weather.js
+++ src/pages/Weather.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './styles/Weather.css';
 
 const Weather = () => {
-    return (
-        <div>
-            <h1>Weather</h1>
-        </div>
-    )
-}
+	return (
+		<div>
+			<h1>Weather</h1>
+			<div className='container-weather'>
+				<div className='temperature'>Temp</div>
+				<div className='temperature'>Temp</div>
+				<div className='temperature'>Temp</div>
+			</div>
+			<div className='container-table-weather'>
+				<div className='table'>Table</div>
+			</div>
+		</div>
+	);
+};
 
-export default Weather
+export default Weather;
