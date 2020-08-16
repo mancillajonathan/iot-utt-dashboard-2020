cf6d44bc67f5dc0291f49597edf4050fc6052c92
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-08-15 21:23:15 -0700

    Nav menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import Menu from './components/Menu';
 function App() {
 	return (
 		<div className='App'>
 			<h1>Iot Dashboard UTT</h1>
+			<Menu />
 		</div>
 	);
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/menu.css';
+
+const Menu = () => {
+    return (
+        <div className='sidenav'>
+            
+        </div>
+    )
+}
+
+export default Menu;
