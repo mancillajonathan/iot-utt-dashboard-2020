abb9823581e9e187f464166e5308fccc54de8894
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-08-15 21:43:18 -0700

    Creating button component

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/button.css';
+
+const Button = () => {
+	return (
+		<div className='Nav-button'>
+			<p>Nav link</p>
+		</div>
+	);
+};
+
+export default Button;
