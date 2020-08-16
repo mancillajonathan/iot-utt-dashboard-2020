4eb84dec62f8cef0e05c91d26c746fe269380028
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-08-15 21:43:48 -0700

    Re-designing nav-link component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './styles/menu.css';
+import NavLink from './NavLinks';
+import Button from './Button';
 
 const Menu = () => {
-    return (
-        <div className='sidenav'>
-            
-        </div>
-    )
-}
+	return (
+		<div className='sidenav'>
+            <Button />
+			<NavLink />
+		</div>
+	);
+};
 
 export default Menu;
---
+++ src/components/NavLinks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/navLink.css';
+
+const NavLinks = () => {
+	return (
+		<div className='Nav-link'>
+			<p>Nav link</p>
+		</div>
+	);
+};
+
+export default NavLinks;
