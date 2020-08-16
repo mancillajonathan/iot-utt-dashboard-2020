bc08a5e8942cb4e294171dac2b1479a6ebc75cee
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-08-15 23:06:25 -0700

    Creating data.json

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,17 +2,22 @@ import React from 'react';
 import './styles/menu.css';
 import NavLink from './NavLinks';
 import Button from './Button';
+import data from '../data.json';
+
+const { navLinks } = data;
 
 const Menu = () => {
 	return (
 		<div className='sidenav'>
 			<Button iconName='fas fa-plus-circle' text='CTA Button' />
-			<NavLink iconName='fas fa-th-large' text='Home' link='/' />
-			<NavLink iconName='fas fa-user-circle' text='User' link='/user'/>
-			<NavLink iconName='fas fa-tachometer-alt' text='Weather' link='/weather' />
-			<NavLink iconName='fas fa-chart-pie' text='Charts' link='/charts' />
-			<NavLink iconName='fas fa-info-circle' text='Help' link='/help' />
-			<NavLink iconName='fas fa-sign-out-alt' text='Log Out' link='/log-out' />
+			{navLinks.map((link) => (
+				<NavLink
+					key={link.key}
+					iconName={link.iconName}
+					text={link.text}
+					link={link.link}
+				/>
+			))}
 		</div>
 	);
 };
