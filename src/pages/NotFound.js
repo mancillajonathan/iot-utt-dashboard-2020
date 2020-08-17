2a344aec58adc673f55e719b31c7f7b102083089
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-08-16 20:13:44 -0700

    Adding not found image

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import notFound from '../images/not-found-404.png';
+import './styles/notFound.css';
 
 const NotFound = () => {
 	return (
 		<div>
-			<h1>Error 404 page not found</h1>
+			<h1>This page is lost in space</h1>
+			<img src={notFound} alt='space' className='notFound__image' />
 		</div>
 	);
 };
