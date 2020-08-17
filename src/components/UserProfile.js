a6c90d03718e106dbfc410deb68057a5689cc56a
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-08-16 20:31:32 -0700

    adding profile picture

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,27 +1,36 @@
 import React, { Fragment } from 'react';
 import './styles/userProfile.css';
+import data from '../data.json';
+import profile from '../images/ui_face.jpg';
+const { userInfo } = data;
 
 const UserProfile = () => {
 	return (
 		<Fragment>
 			<div className='userProfile__container'>
 				<div className='userProfile__image-container'>
-					<img src='#' alt='' />
+					<img src={profile} alt='profile' />
 				</div>
 				<div className='userProfile__title-container'>
-					<h2>John Smith</h2>
-					<p className='userProfile__job-title'>Frotend Developer</p>
+					<h2>{userInfo.name}</h2>
+					<p className='userProfile__job-title'>
+						{' '}
+						{userInfo.role} <i class='fas fa-check-circle'></i>
+					</p>
 				</div>
 			</div>
 			<div className='userProfile__buttons-container'>
 				<div className='buttons'>
-					<i class='fas fa-calendar-check'></i><span>Check dates</span>
+					<i class='fas fa-calendar-check'></i>
+					<span>Check dates</span>
 				</div>
 				<div className='buttons'>
-					<i class='fas fa-user-plus'></i><span>New user</span>
+					<i class='fas fa-user-plus'></i>
+					<span>New user</span>
 				</div>
 				<div className='buttons'>
-					<i class='fas fa-tasks'></i><span>My tasks</span>
+					<i class='fas fa-tasks'></i>
+					<span>My tasks</span>
 				</div>
 			</div>
 		</Fragment>
